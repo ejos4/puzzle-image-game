e8b8127f39109d98f796a72e94ded24cb75aca98
ejos4 <erwanjos448@gmail.com>
2022-09-12 22:14:24 +0200

    Nightmare need to clean it, but it seams to works finnaly!

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const HEIGHT = canvas.height = 600;
 
 canvas.style.background = "lightgrey";
 
-const factor = 5; //How many times the is split for a horizontal and vertical orientation (Ex: factor of 2 means that the image is split 2 times (1 vertical and 1 horizontal))
+const factor = 2; //How many times the is split for a horizontal and vertical orientation (Ex: factor of 2 means that the image is split 2 times (1 vertical and 1 horizontal))
 const squareSize = WIDTH/factor; // The segment size of one square according to the factor
 // const spaceVal = 0;
 
@@ -20,41 +20,50 @@ function createImg(imgSrc){
 // -------------- Class --------------
 
 class ImageCell{
-    constructor(imgSrc, offsetStep, colSize, rowSize, targetPos, targetWidth, targetHeight) {
+    static switchPosition(imgCellElt1, imgCellElt2) {
+        let tempPos = imgCellElt1.position;
+        let tempId = imgCellElt1.id;
+        imgCellElt1.position = imgCellElt2.position;
+        // imgCellElt1.id = imgCellElt2.id;
+        imgCellElt2.position = tempPos;
+        // imgCellElt2.id = tempId;
+    }
+
+    constructor(imgSrc, cellId, offsetStep, colSize, rowSize, targetPos, targetWidth, targetHeight) {
         this.img = imgSrc;
+        this.id = cellId;
         this.offsetStep = offsetStep;
         this.colSize = colSize;
         this.rowSize = rowSize;
         this.position = targetPos;
         this.width = targetWidth;
         this.height = targetHeight;
-        // this.isHide = false;
+        this.isHide = false;
     }
 
     draw(ctx) {
-        ctx.drawImage(
-            this.img,
-            (this.img.width*this.offsetStep.x)/this.colSize,// offset step x
-            (this.img.height*this.offsetStep.y)/this.rowSize,// offset step y
-            this.img.width/this.colSize,//img portion width
-            this.img.height/this.rowSize,//img portion height
-            this.position.x,//target pos X
-            this.position.y,//target pos Y
-            this.width,//target width
-            this.height//target height
-        )
+        if(this.isHide){
+            ctx.fillStyle = "black";
+            ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+        } else {
+            ctx.drawImage(
+                this.img,
+                (this.img.width*this.offsetStep.x)/this.colSize,// offset step x
+                (this.img.height*this.offsetStep.y)/this.rowSize,// offset step y
+                this.img.width/this.colSize,//img portion width
+                this.img.height/this.rowSize,//img portion height
+                this.position.x,//target pos X
+                this.position.y,//target pos Y
+                this.width,//target width
+                this.height//target height
+            );
+        }
     }
 
-    // hide(ctx) {
-    //     if(this.isHide){
-    //         this.draw(ctx);
-    //         this.isHide = false;
-    //     } else {
-    //         ctx.fillStyle = "black";
-    //         ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-    //         this.isHide = true;
-    //     }
-    // }
+    toggleHide() {
+        if(this.isHide){this.isHide = false;} 
+        else {this.isHide = true;}
+    }
 
     isClicked(clickPos) {
         // console.log(this.offsetPos.x+"/"+this.offsetPos.y);
@@ -65,6 +74,7 @@ class ImageCell{
 
 class ImageGrid{
     constructor(imgSrc, numberOfCols, numberOfRows, width, height, startPos = {x: 0, y: 0}){
+        this.startGame = false;
         this.position = startPos;
         this.img = createImg(imgSrc);
         this.numberOfCols = numberOfCols;
@@ -77,7 +87,8 @@ class ImageGrid{
             let currentRow = new Array();
             for(let x = 0; x < numberOfCols; x++){
                 let targetPos = {x: (targetWidth)*x, y: (targetHeight)*y};
-                currentRow.push(new ImageCell(this.img, {x: x, y: y}, numberOfCols, numberOfRows, targetPos, targetWidth, targetHeight));
+                currentRow.push(new ImageCell(this.img, (y*numberOfCols+x), {x: x, y: y}, numberOfCols, numberOfRows, targetPos, targetWidth, targetHeight));
+                if(y === (numberOfRows - 1) && x === (numberOfCols -1)) currentRow[x].toggleHide();
             }
             this.imgGrid.push(currentRow);
         }
@@ -89,20 +100,126 @@ class ImageGrid{
 
     draw(ctx){
         for (let y=0; y<this.numberOfRows; y++){
-            for (let x=0; x<this.numberOfRows; x++){
+            for (let x=0; x<this.numberOfCols; x++){
                 this.drawCell(ctx, {x: x, y: y});
             }
         }
     }
 
-    handleClick(ctx, clickPos){
+    handleClick(clickPos){
         for (let y=0; y<this.numberOfRows; y++){
-            for (let x=0; x<this.numberOfRows; x++){
-                if (this.imgGrid[y][x].isClicked(clickPos)) {
-                    // this.imgGrid[y][x].hide(ctx);
+            for (let x=0; x<this.numberOfCols; x++){
+                let cell = this.imgGrid[y][x];
+                if (cell.isClicked(clickPos)) {
+                    this.checkNeighbours(y, x);
                 }
             }
         }
+    }
+
+    // Paramaters of type ({GridCoord: {x, y}, cell: cellElt},{GridCoord: {x, y}, cell: cellElt})
+    switchCells(data1, data2) {
+        let cell1 = data1.cell;
+        let cell2 = data2.cell;
+        let pos1 = data1.gridPos;
+        let pos2 = data2.gridPos;
+
+        ImageCell.switchPosition(cell1, cell2);
+        // console.log("Cell 1 :",cell1);
+        // console.log("Cell 2 :",cell2);
+        let temp = cell1;
+        this.imgGrid[pos1.y][pos1.x] = this.imgGrid[pos2.y][pos2.x];
+        this.imgGrid[pos2.y][pos2.x] = temp;
+    }
+
+    checkNeighbours(col, row){
+        // for (let y=0; y<this.numberOfRows; y++){
+        //     for (let x=0; x<this.numberOfCols; x++){
+        //         let cell = this.imgGrid[y][x];
+        //         if (cell.isClicked(clickPos)) {
+        //             this.checkNeighbours(cell.position);
+        //         }
+        //     }
+        // }
+        let cell = this.imgGrid[col][row];
+        let hiddenCell = null;
+        let hiddenCellGridCoord = {};
+        console.log(cell, col, row);
+
+        if(cell.isHide) return;
+        
+        if((col < this.numberOfCols-1) && this.imgGrid[col+1][row].isHide) {
+            console.log("Switch 1");
+            hiddenCell = this.imgGrid[col+1][row];
+            hiddenCellGridCoord['x'] = row;
+            hiddenCellGridCoord['y'] = col+1;
+            // return this.switchCells(cell, this.imgGrid[col+1][row]);
+        }
+        
+        if((col >= 1) && this.imgGrid[col-1][row].isHide) {
+            console.log("Switch 2");
+            hiddenCell = this.imgGrid[col-1][row];
+            hiddenCellGridCoord['x'] = row;
+            hiddenCellGridCoord['y'] = col-1;
+            // return this.switchCells(cell, this.imgGrid[col-1][row]);
+        }
+        
+        if((row < this.numberOfRows-1) && this.imgGrid[col][row+1].isHide) {
+            console.log("Switch 3");
+            hiddenCell = this.imgGrid[col][row+1];
+            hiddenCellGridCoord['x'] = row+1;
+            hiddenCellGridCoord['y'] = col;
+            // return this.switchCells(cell, this.imgGrid[col][row+1]);
+        }
+        
+        if((row >= 1) && this.imgGrid[col][row-1].isHide) {
+            console.log("Switch 4");
+            hiddenCell = this.imgGrid[col][row-1];
+            hiddenCellGridCoord['x'] = row-1;
+            hiddenCellGridCoord['y'] = col;
+            // return this.switchCells(cell, this.imgGrid[col][row-1]);
+        }
+
+        if(hiddenCell !== null){
+            let pos1 = {x:row,y:col};
+            this.switchCells({gridPos: pos1, cell: cell}, {gridPos: hiddenCellGridCoord, cell: hiddenCell});
+        }
+    }
+
+    shuffleGrid(n) {
+        if(n === 0) {
+            this.startGame = true;
+            return;
+        }
+        let pos1 = {
+            x: Math.floor(Math.random()*this.numberOfCols),
+            y: Math.floor(Math.random()*this.numberOfRows)
+        }
+        let pos2 = {
+            x: Math.floor(Math.random()*this.numberOfCols),
+            y: Math.floor(Math.random()*this.numberOfRows)
+        }
+        let cell1 = this.imgGrid[pos1.y][pos1.x];
+        let cell2 = this.imgGrid[pos2.y][pos2.x];
+        // console.log("Cell 1 :",cell1);
+        // console.log("Cell 2 :",cell2);
+        this.switchCells({gridPos: pos1, cell: cell1}, {gridPos: pos2, cell: cell2});
+        // setTimeout(()=>{
+        // }, 3000);
+
+        this.shuffleGrid(n-1);
+    }
+
+    verifyGrid() {
+        let supposedId = 0;
+        for (let y=0; y<this.numberOfRows; y++){
+            for (let x=0; x<this.numberOfCols; x++){
+                let currentCell = this.imgGrid[y][x];
+                if(currentCell.id !== supposedId++) return false;
+            }
+        }
+
+        return true;
     }
 }
 
@@ -112,48 +229,27 @@ const imgURL = 'https://images.unsplash.com/photo-1662981095676-cd01749247f4?ixl
 const imgURL2 = 'https://i.picsum.photos/id/944/200/200.jpg?hmac=1Hdj8yjDsg6pbmgsiAGRdUQ8MA4hfi4uapepYyrMaGU';
 
 
-
-
-// Create 4 image segment;
-// let imgArr = new Array();
-
-// for (let i=0; i<4; i++){
-//     let position;
-//     let offsetStep;
-//     switch(i){
-//         case 0: 
-//             position={x:0,y:0};
-//             offsetStep={x:0,y:0};
-//             break;
-
-//         case 1: 
-//             position={x:squareSize,y:0};
-//             offsetStep={x:1,y:0};
-//             break;
-
-//         case 2: 
-//             position={x:0,y:squareSize};
-//             offsetStep={x:0,y:1};
-//             break;
-
-//         case 3: 
-//             position={x:squareSize,y:squareSize};
-//             offsetStep={x:1,y:1};
-//             break;
-
-//         default:
-//             break;
-//     }
-//     let img = new ImageCell(imgURL2, offsetStep, 2, 2, position, squareSize, squareSize);
-//     imgArr.push(img);
-// }
-
-// for(let i = 0; i< imgArr.length; i++){
-//     let img = imgArr[i];
-//     img.draw(context);
-// }
 let gridImg = new ImageGrid(imgURL, factor, factor, WIDTH, HEIGHT);
-gridImg.draw(context);
+
+    gridImg.shuffleGrid(10);
+// setTimeout(() => {
+//     gridImg.shuffleGrid(10);
+//     gridImg.shuffleGrid(10);
+// }, 3000);
+let startGame = false;
+function loop(){
+    context.clearRect(0, 0, WIDTH, HEIGHT);
+    gridImg.draw(context);
+
+    // gridImg.shuffleGrid(1);
+    // gridImg.shuffleGrid(10);
+    
+    if(gridImg.startGame && gridImg.verifyGrid()){
+        alert("Bravo puzzle complet");
+    }
+    window.requestAnimationFrame(loop);   
+}
+loop();
 
 
 // Handle click on canvas
@@ -161,10 +257,5 @@ canvas.addEventListener('click', (e) => {
     const rect = canvas.getBoundingClientRect();
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
-    gridImg.handleClick(context, {x: x, y: y});
-    // for(let i = 0; i< imgArr.length; i++){
-    //     let img = imgArr[i];
-    //     imgClicked = img.isClicked({x: x, y: y});
-    //     if (imgClicked) console.log(`L'image n°${i+1} isClick`);
-    // }
+    gridImg.handleClick({x: x, y: y});
 })
