620ee36ad57239d50939ec1dd8081d3e2b2f7611
ejos4 <erwanjos448@gmail.com>
2022-09-12 19:21:15 +0200

    Adding a new class ImageGrid that manage the array of imageCell (previously imageSection)

--- script.js
+++ script.js
@@ -7,92 +7,54 @@ const HEIGHT = canvas.height = 600;
 
 canvas.style.background = "lightgrey";
 
-const factor = 2;
-const squareSize = WIDTH/factor;
-const spaceVal = 0;
+const factor = 5; //How many times the is split for a horizontal and vertical orientation (Ex: factor of 2 means that the image is split 2 times (1 vertical and 1 horizontal))
+const squareSize = WIDTH/factor; // The segment size of one square according to the factor
+// const spaceVal = 0;
 
+function createImg(imgSrc){
+    let img = new Image();
+    img.src = imgSrc;
+    return img;
+}
 
 // -------------- Class --------------
-class ImageSection{
-    constructor(imgSrc, offsetStep, sizeFactor, targetPos, targetWidth, targetHeight) {
-        this.img = new Image();
-        this.img.src = imgSrc;
+
+class ImageCell{
+    constructor(imgSrc, offsetStep, colSize, rowSize, targetPos, targetWidth, targetHeight) {
+        this.img = imgSrc;
         this.offsetStep = offsetStep;
-        this.factor = sizeFactor;
-        this.targetPos = targetPos;
+        this.colSize = colSize;
+        this.rowSize = rowSize;
+        this.position = targetPos;
         this.width = targetWidth;
         this.height = targetHeight;
+        // this.isHide = false;
     }
 
     draw(ctx) {
         ctx.drawImage(
             this.img,
-            (this.img.width*this.offsetStep.x)/this.factor,// offset step x
-            (this.img.height*this.offsetStep.y)/this.factor,// offset step y
-            this.img.width/this.factor,//img portion width
-            this.img.height/this.factor,//img portion height
-            this.targetPos.x,//target pos X
-            this.targetPos.y,//target pos Y
+            (this.img.width*this.offsetStep.x)/this.colSize,// offset step x
+            (this.img.height*this.offsetStep.y)/this.rowSize,// offset step y
+            this.img.width/this.colSize,//img portion width
+            this.img.height/this.rowSize,//img portion height
+            this.position.x,//target pos X
+            this.position.y,//target pos Y
             this.width,//target width
             this.height//target height
         )
     }
 
-// ndraw(context){
-//     // Image 1
-//     context.drawImage(
-//         imgCls.img,
-//         0,// offset step x
-//         0,// offset step y
-//         imgCls.img.width/factor,//img portion width
-//         imgCls.img.height/factor,//img portion height
-//         0, //target pos X
-//         0,//target pos Y
-//         WIDTH/factor,//target width
-//         HEIGHT/factor,//target height
-//     );
-//     // Image 2
-//     context.drawImage(
-//         imgCls.img,
-//         imgCls.img.width/factor,// offset step x
-//         0,// offset step y
-//         imgCls.img.width/factor,//img portion width
-//         imgCls.img.height/factor,//img portion height
-//         WIDTH/factor,//target pos X
-//         0, //target pos Y
-//         WIDTH/factor,//target width
-//         HEIGHT/factor,//target height
-//     );
-// }
-}
-
-class ImageSegment{
-    constructor(imgSrc, position, width, height, offsetPos={x:0, y:0}, maxPos={x:factor,y:factor}){
-        this.img = new Image();
-        this.img.src = imgSrc;
-        this.position = {x: position.x, y: position.y};
-        this.width = width;
-        this.height = height;
-        this.offsetPos = offsetPos;
-    }
-
-    draw(ctx){
-        // console.log()
-        ctx.drawImage(
-            this.img, 
-            // 0,0,
-            this.offsetPos.x,
-            this.offsetPos.y,
-            this.offsetPos.x+(this.img.width/factor),
-            this.offsetPos.y+(this.img.height/factor)*2,
-            // this.img.width,
-            // this.img.height,
-            this.position.x, 
-            this.position.y,
-            this.width,
-            this.height,
-        );
-    }
+    // hide(ctx) {
+    //     if(this.isHide){
+    //         this.draw(ctx);
+    //         this.isHide = false;
+    //     } else {
+    //         ctx.fillStyle = "black";
+    //         ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    //         this.isHide = true;
+    //     }
+    // }
 
     isClicked(clickPos) {
         // console.log(this.offsetPos.x+"/"+this.offsetPos.y);
@@ -100,6 +62,50 @@ class ImageSegment{
         return false;
     }
 }
+
+class ImageGrid{
+    constructor(imgSrc, numberOfCols, numberOfRows, width, height, startPos = {x: 0, y: 0}){
+        this.position = startPos;
+        this.img = createImg(imgSrc);
+        this.numberOfCols = numberOfCols;
+        this.numberOfRows = numberOfRows;
+        this.imgGrid = new Array();
+        let targetWidth = width/numberOfCols,
+            targetHeight = height/numberOfRows;
+
+        for(let y=0; y < numberOfRows; y++) {
+            let currentRow = new Array();
+            for(let x = 0; x < numberOfCols; x++){
+                let targetPos = {x: (targetWidth)*x, y: (targetHeight)*y};
+                currentRow.push(new ImageCell(this.img, {x: x, y: y}, numberOfCols, numberOfRows, targetPos, targetWidth, targetHeight));
+            }
+            this.imgGrid.push(currentRow);
+        }
+    }
+
+    drawCell(ctx, position){
+        this.imgGrid[position.y][position.x].draw(ctx);
+    }
+
+    draw(ctx){
+        for (let y=0; y<this.numberOfRows; y++){
+            for (let x=0; x<this.numberOfRows; x++){
+                this.drawCell(ctx, {x: x, y: y});
+            }
+        }
+    }
+
+    handleClick(ctx, clickPos){
+        for (let y=0; y<this.numberOfRows; y++){
+            for (let x=0; x<this.numberOfRows; x++){
+                if (this.imgGrid[y][x].isClicked(clickPos)) {
+                    // this.imgGrid[y][x].hide(ctx);
+                }
+            }
+        }
+    }
+}
+
 
 // -------------- Start display --------------
 const imgURL = 'https://images.unsplash.com/photo-1662981095676-cd01749247f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80';
@@ -109,83 +115,56 @@ const imgURL2 = 'https://i.picsum.photos/id/944/200/200.jpg?hmac=1Hdj8yjDsg6pbmg
 
 
 // Create 4 image segment;
-let imgArr = new Array();
+// let imgArr = new Array();
 
-for (let i=0; i<3; i++){
-    let position;
-    let offsetStep;
-    switch(i){
-        case 0: 
-            position={x:0,y:0};
-            offsetStep={x:0,y:0};
-            break;
+// for (let i=0; i<4; i++){
+//     let position;
+//     let offsetStep;
+//     switch(i){
+//         case 0: 
+//             position={x:0,y:0};
+//             offsetStep={x:0,y:0};
+//             break;
 
-        case 1: 
-            position={x:squareSize,y:0};
-            offsetStep={x:1,y:0};
-            break;
+//         case 1: 
+//             position={x:squareSize,y:0};
+//             offsetStep={x:1,y:0};
+//             break;
 
-        case 2: 
-            position={x:0,y:squareSize};
-            offsetStep={x:0,y:1};
-            break;
+//         case 2: 
+//             position={x:0,y:squareSize};
+//             offsetStep={x:0,y:1};
+//             break;
 
-        case 3: 
-            position={x:squareSize,y:squareSize};
-            offsetStep={x:1,y:1};
-            break;
+//         case 3: 
+//             position={x:squareSize,y:squareSize};
+//             offsetStep={x:1,y:1};
+//             break;
 
-        default:
-            break;
-    }
-    let img = new ImageSection(imgURL, offsetStep, factor, position, squareSize, squareSize);
-    imgArr.push(img);
-}
+//         default:
+//             break;
+//     }
+//     let img = new ImageCell(imgURL2, offsetStep, 2, 2, position, squareSize, squareSize);
+//     imgArr.push(img);
+// }
 
-for(let i = 0; i< imgArr.length; i++){
-    let img = imgArr[i];
-    img.draw(context);
-}
-// let imgCls = imgArr[0];
+// for(let i = 0; i< imgArr.length; i++){
+//     let img = imgArr[i];
+//     img.draw(context);
+// }
+let gridImg = new ImageGrid(imgURL, factor, factor, WIDTH, HEIGHT);
+gridImg.draw(context);
 
-
-
-
-
-// Image 3
-// context.drawImage(
-//     imgCls.img,
-//     0,
-//     0,
-//     imgCls.img.width,
-//     imgCls.img.height,
-//     0, 
-//     0,
-//     WIDTH,
-//     HEIGHT,
-// );
-
-// Image 4
-// context.drawImage(
-//     imgCls.img,
-//     0,
-//     0,
-//     imgCls.img.width,
-//     imgCls.img.height,
-//     0, 
-//     0,
-//     WIDTH,
-//     HEIGHT,
-// );
 
 // Handle click on canvas
 canvas.addEventListener('click', (e) => {
     const rect = canvas.getBoundingClientRect();
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
-    for(let i = 0; i< imgArr.length; i++){
-        let img = imgArr[i];
-        imgClicked = img.isClicked({x: x, y: y});
-        if (imgClicked) console.log(`L'image n°${i+1} isClick`);
-    }
+    gridImg.handleClick(context, {x: x, y: y});
+    // for(let i = 0; i< imgArr.length; i++){
+    //     let img = imgArr[i];
+    //     imgClicked = img.isClicked({x: x, y: y});
+    //     if (imgClicked) console.log(`L'image n°${i+1} isClick`);
+    // }
 })
